6538a74f4a0397e2bc70322699bae8cb24fa5592
Vladimir <fagoti.sm@gmail.com>
2025-08-25 12:49:25 +0800

    Готово.

--- opinions_app/api_view.py
+++ opinions_app/api_view.py
@@ -3,6 +3,17 @@ from flask import jsonify, request
 from . import app, db
 from .models import Opinion
 from .views import random_opinion
+from .error_handlers import InvalidAPIUsage
+
+
+def get_opinion_or_raise(id):
+    opinion = Opinion.query.get(id)
+    if opinion is None:
+        raise InvalidAPIUsage(
+            f'Мнение с указанным "id={id}" не найдено',
+            404
+        )
+    return opinion
 
 
 @app.route('/api/opinions', methods=['GET'])
@@ -15,8 +26,12 @@ def get_opinions():
 @app.route('/api/opinions', methods=['POST'])
 def add_opinions():
     data = request.get_json(silent=True)
-    if data or 'title' not in data or 'text' not in data:
-        return
+    if not data or 'title' not in data or 'text' not in data:
+        raise InvalidAPIUsage('В запросе отсутствует обязательные поля')
+
+    if Opinion.query.filter_by(text=data['text']).first():
+        raise InvalidAPIUsage('Такое мнение уже есть в базе данных.')
+
     opinion = Opinion()
     opinion.from_dict(data)
     db.session.add(opinion)
@@ -27,14 +42,23 @@ def add_opinions():
 @app.route('/api/opinions/<int:id>', methods=['GET'])
 def get_opinion(id):
     """"""
-    opinion = Opinion.query.get_or_404(id)
+    opinion = get_opinion_or_raise()
     return jsonify({'opinion': opinion.to_dict()})
 
 
 @app.route('/api/opinions/<int:id>', methods=['PATCH'])
 def update_opinion(id):
     data = request.get_json()
-    opinion = Opinion.query.get_or_404(id)
+    if (
+            'text' not in data
+            and Opinion.query.filter_by(text=data['text'])
+    ):
+        return jsonify(
+            {
+                'error': 'Такое мнение уже есть в базе данных.'
+            }
+        )
+    opinion = get_opinion_or_raise()
     opinion.title = data.get('title', opinion.title)
     opinion.text = data.get('text', opinion.text)
     opinion.source = data.get('source', opinion.source)
@@ -45,7 +69,7 @@ def update_opinion(id):
 
 @app.route('/api/opinions/<int:id>', methods=['DELETE'])
 def delete_opinions(id):
-    opinion = Opinion.query.get_or_404(id)
+    opinion = get_opinion_or_raise()
     db.session.delete(opinion)
     db.session.commit()
     return '', 204
@@ -54,4 +78,9 @@ def delete_opinions(id):
 @app.route('/api/get-random-opinion/', methods=['GET'])
 def ger_random_opinion():
     opinion = random_opinion()
+    if opinion is None:
+        raise InvalidAPIUsage(
+            f'В базе данных нет мнений.',
+            404
+        )
     return jsonify({'opinion': opinion.to_dict()})
